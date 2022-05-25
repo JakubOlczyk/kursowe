faf79ce4835b0941ece438962ecb6fdf540f34e4
JakubOlczyk <105982658+JakubOlczyk@users.noreply.github.com>
2022-05-25 17:19:37 +0200

    Delete java001 directory

--- java001/src/com/example/basics/data/types/J05_variableNames.java
+++
@@ -1,9 +0,0 @@
-package com.example.basics.data.types;
-
-public class J05_variableNames {
-    public static void main(String[] args) {
-        int currentHour = 12;
-        float moneyInThePocket = 23.12f;
-        double $speed = 100.10d;
-    }
-}
